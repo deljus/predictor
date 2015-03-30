43018e1e124cc1a3c6385a262933596b999cf2b6
Ramil Nugmanov <stsouko@live.ru>
2015-03-30 10:53:03 +0300

    test

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from .config import REQ_MAPPING
 from flask import render_template, url_for, redirect
 from app import app
 from flask.ext.restful import reqparse, abort, Api, Resource, fields, marshal
-from flask.ext import excel
+#from flask.ext import excel
 #import pyexcel.ext.xls
 #import pyexcel.ext.xlsx
 
