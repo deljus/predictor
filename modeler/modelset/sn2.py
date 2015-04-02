f9f0f2956e1918a6167afbd3a9d7570f13a7b00e
Ramil Nugmanov <stsouko@live.ru>
2015-04-03 02:32:13 +0300

    добавил лицензию.

--- modeler/modelset/sn2.py
+++ modeler/modelset/sn2.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright 2015 Ramil Nugmanov <stsouko@live.ru>
+# This file is part of PREDICTOR.
+#
+# PREDICTOR is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
 import os
 import pickle
-import random
 import time
-
-__author__ = 'stsouko'
 from modelset import register_model, chemaxpost
 import subprocess as sp
-from sklearn import svm
 
 
 class Model():
@@ -46,7 +61,6 @@ class Model():
         return hashlist
 
     def getresult(self, chemical):
-        """do some operations on chemical"""
         data = {"structure": chemical['structure'], "parameters": "rxn"}
         structure = chemaxpost('calculate/stringMolExport', data)
         temperature = chemical['temperature'] if chemical['temperature'] else 298
@@ -82,6 +96,7 @@ class Model():
                 os.remove(temp_file_rxn)
                 os.remove(temp_file_sdf)
                 os.remove(temp_file_csv + '.csv')
+                os.remove(temp_file_csv + '.hdr')
 
             return result
         else:
