d74f312b57e7ba27b8121264e7cb2077a0e4b0f2
Ramil Nugmanov <stsouko@live.ru>
2015-04-20 15:10:26 +0300

    fixed

--- modeler/modelset/martas.py
+++ modeler/modelset/martas.py
@@ -103,8 +103,7 @@ class Model(consensus_dragos, standardize_dragos, ISIDAatommarker):
                 for x in files:
                     if 'structure-%d' % fixtime in x:
                         try:
-                            pass
-                            #os.remove(os.path.join(self.modelpath, x))
+                            os.remove(os.path.join(self.modelpath, x))
                         except:
                             print('something is very bad. file %s undeletable' % x)
 
