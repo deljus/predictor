181584553ce49e1a5ec3545c3079f31e35c3d1ac
Ramil Nugmanov <stsouko@live.ru>
2016-01-03 12:47:07 +0300

    маппер подправил. он корявый в батч режиме(слишком много запросов в кемаксон.), но да ладно.

--- utils/mappercore.py
+++ utils/mappercore.py
@@ -25,8 +25,6 @@ import subprocess as sp
 import xml.etree.ElementTree as ET
 import re
 import json
-
-#TODO
 from utils.FEAR.RDFread import RDFread
 from utils.FEAR.CGR import CGR
 fear = CGR()
@@ -37,55 +35,60 @@ def create_task_from_file(task):
     task_id = task['id']
     tmp_file = '%stmp-%d.mrv' % (UPLOAD_PATH, task_id)
     tmp_fear_file = '%stmp-%d.rxn' % (UPLOAD_PATH, task_id)
+    serverput("task_status/%s" % task_id, {'task_status': LOCK_MAPPING})
     sp.call([STANDARDIZER, file_path, '-c', STANDARD, '-f', 'mrv', '-o', tmp_file])
     solv = {x['name'].lower(): x['id'] for x in getsolvents()}
 
     with open(tmp_file, 'r') as file:
         for mol in file:
             if '<MDocument>' in mol:
-                tree = ET.fromstring(mol)
-                prop = {x.get('title').lower(): x.find('scalar').text.lower().strip() for x in tree.iter('property')}
-                solvlist = {}
-                temp = 298
-                for i, j in prop.items():
-                    if 'solvent.amount.' in i:
-                        k = re.split('[:=]', j)
-                        id = solv.get(k[0].strip())
-                        if id:
-                            if '%' in k[-1]:
-                                v = k[-1].replace('%', '')
-                                grader = 100
-                            else:
-                                v = k[-1]
-                                grader = 1
+                try:
+                    tree = ET.fromstring(mol)
+                    prop = {x.get('title').lower(): x.find('scalar').text.lower().strip()
+                            for x in tree.iter('property')}
+                    solvlist = {}
+                    temp = 298
+                    for i, j in prop.items():
+                        if 'solvent.amount.' in i:
+                            k = re.split('[:=]', j)
+                            id = solv.get(k[0].strip())
+                            if id:
+                                if '%' in k[-1]:
+                                    v = k[-1].replace('%', '')
+                                    grader = 100
+                                else:
+                                    v = k[-1]
+                                    grader = 1
+                                try:
+                                    v = float(v) / grader
+                                except ValueError:
+                                    v = 1
+                                solvlist[id] = v
+                        elif 'temperature' == i:
                             try:
-                                v = float(v) / grader
+                                temp = float(j)
                             except ValueError:
-                                v = 1
-                            solvlist[id] = v
-                    elif 'temperature' == i:
-                        try:
-                            temp = float(j)
-                        except ValueError:
-                            temp = 298
+                                temp = 298
 
-                data = {"structure": mol.rstrip(), "parameters": {"method": "DEHYDROGENIZE"}}
-                structure = chemaxpost('convert/hydrogenizer', data)
-                if structure:
-                    data = dict(task_id=task_id, structure=structure, solvents=json.dumps(solvlist), temperature=temp)
-                    q = serverpost('parser', data)
-                    if q.isdigit(): # проверка на корректный ответ. по сути не нужна. но да пох.
-                        data = {"structure": mol, "parameters": "rxn"}
-                        structure = chemaxpost('calculate/stringMolExport', data)
-                        if '$RXN' in structure:
-                            with open(tmp_fear_file, 'w') as tmp:
-                                tmp.write(structure)
-                            FEAR(tmp_fear_file, int(q))
-                            os.remove(tmp_fear_file)
-                        else:
-                            pass
-                            # todo: тут надо для молекул заморочиться.
-
+                    data = {"structure": mol.rstrip(), "parameters": {"method": "DEHYDROGENIZE"}}
+                    structure = chemaxpost('convert/hydrogenizer', data)
+                    if structure:
+                        data = dict(task_id=task_id, structure=structure, solvents=json.dumps(solvlist),
+                                    temperature=temp)
+                        q = serverpost('parser', data)
+                        if q.isdigit(): # проверка на корректный ответ. по сути не нужна. но да пох.
+                            data = {"structure": mol, "parameters": "rxn"}
+                            structure = chemaxpost('calculate/stringMolExport', data)
+                            if '$RXN' in structure:
+                                with open(tmp_fear_file, 'w') as tmp:
+                                    tmp.write(structure)
+                                FEAR(tmp_fear_file, int(q))
+                                os.remove(tmp_fear_file)
+                            else:
+                                pass
+                                # todo: тут надо для молекул заморочиться.
+                except:
+                    pass
     os.remove(tmp_file)
     serverput("task_status/%s" % task_id, {'task_status': MAPPING_DONE})
 
