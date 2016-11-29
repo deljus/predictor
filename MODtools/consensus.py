a8fc1ed1514f446db3f61e52f6cf097d9df4a71f
stsouko <stsouko@live.ru>
2016-11-29 14:59:13 +0300

    BUGFIX. consensus refactored.

--- MODtools/consensus.py
+++ MODtools/consensus.py
@@ -18,69 +18,9 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #
-from math import sqrt, ceil
-import numpy as np
 
 
 class ConsensusDragos(object):
-    def __init__(self):
-        self.__TRUST = 5
-        self.__INlist = []
-        self.__ALLlist = []
-
-    def cumulate(self, P, AD):
-        if AD:
-            self.__INlist.append(P)
-        self.__ALLlist.append(P)
-
-    def report(self):
-        if not self.__ALLlist:
-            return False  # break if all models fails to predict
-
-        reason = []
-        result = []
-        INarr = np.array(self.__INlist)
-        ALLarr = np.array(self.__ALLlist)
-
-        PavgALL = ALLarr.mean()
-        sigmaALL = sqrt(ALLarr.var())
-
-        if self.__INlist:
-            PavgIN = INarr.mean()
-            sigmaIN = sqrt(INarr.var())
-            pavgdiff = abs(PavgIN - PavgALL)
-            if pavgdiff > self.TOL:
-                reason.append(self.__errors['diff'] % pavgdiff)
-                self.__TRUST -= 1
-        else:
-            self.__TRUST -= 1
-            reason.append(self.__errors['zad'])
-
-        proportion = len(self.__INlist) / len(self.__ALLlist)
-        if proportion > self.Nlim:
-            sigma = sigmaIN
-            Pavg = PavgIN
-        else:
-            sigma = sigmaALL
-            Pavg = PavgALL
-            self.__TRUST -= 1
-            if self.__INlist:
-                reason.append(self.__errors['lad'] % ceil(100 * proportion))
-
-        proportion = sigma / self.TOL
-        if proportion > 1:
-            self.__TRUST -= int(proportion)
-            reason.append(self.__errors['stp'] % (proportion * 100 - 100))
-
-        result.append(dict(type='text',
-                           attrib='predicted value ± sigma%s' % (' (%s)' % self.units if self.units else ''),
-                           value='%.2f ± %.2f' % (Pavg, sigma)))
-        result.append(dict(type='text', attrib='prediction trust', value=self.__trustdesc.get(self.__TRUST, 'None')))
-        if reason:
-            result.append(dict(type='text', attrib='reason', value='. '.join(reason)))
-
-        return result
-
     errors = dict(lad='Too few (less than %d %%) local models have applicability domains covering this structure',
                   diff='The other local models disagree (prediction value difference = %.2f) with the prediction of '
                        'the minority containing structure inside their applicability domain',
@@ -88,4 +28,4 @@ class ConsensusDragos(object):
                       'of the property range width',
                   zad='None of the local models have applicability domains covering this structure')
 
-    __trustdesc = {5: 'Optimal', 4: 'Good', 3: 'Medium', 2: 'Low'}
+    trust_desc = {5: 'Optimal', 4: 'Good', 3: 'Medium', 2: 'Low', 1: 'Bad'}
